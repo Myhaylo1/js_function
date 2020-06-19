588d84b7f12cdef99e0001e2f36a19eeda21511c
Myhaylo1 <zpkazka@gmail.com>
2020-06-19 17:07:02 +0300

    Add function checkMultiplicity().

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function isAdult(x)
 }
 console.log('isAdult(20)=',isAdult(20));
 console.log('isAdult(4)=',isAdult(4));
+
+/**
+ *
+ * @param a
+ * @param b
+ * @returns {boolean}: a%b=0
+ */
+function checkMultiplicity(a,b)
+{
+  return (a%b)===0;
+}
+console.log('checkMultiplicity(25,5)=',checkMultiplicity(25,5));
+console.log('checkMultiplicity(15,3)=',checkMultiplicity(15,3));
+console.log('checkMultiplicity(15,5)=',checkMultiplicity(15,5));
+console.log('checkMultiplicity(15,4)=',checkMultiplicity(15,4));
