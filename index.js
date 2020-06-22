6a0765f2b92c36515a24fc22610756a79e34f744
Myhaylo1 <zpkazka@gmail.com>
2020-06-22 17:08:09 +0300

    Add function triangle().

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ console.log('checkMultiplicity(25,5)=',checkMultiplicity(25,5));
 console.log('checkMultiplicity(15,3)=',checkMultiplicity(15,3));
 console.log('checkMultiplicity(15,5)=',checkMultiplicity(15,5));
 console.log('checkMultiplicity(15,4)=',checkMultiplicity(15,4));
+
+/**
+ *
+ * @param a
+ * @param b
+ * @param c
+ * @returns {boolean}
+ */
+function triangle(a,b,c)
+{
+  return (((a+b)>c) && ((a+c)>b) && ((c+b)>a));
+}
+console.log('triangle(3,4,5)=',triangle(3,4,5));
+console.log('triangle(3,4,9)=',triangle(3,4,9));
